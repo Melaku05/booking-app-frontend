39789686b19ad4b534037aa52cdbf134be809c31
Ismail Courr <contact@ismailcourr.ml>
2022-08-19 15:33:20 +0100

    Clean the app and remove hard coded code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import doctorReducer from './doctors/doctors';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    doctors: doctorReducer,
   },
 });
