d6f690afbb7918f7c818d11a7237c92d9e1b5319
aboongm <aboongm@yahoo.com>
2022-08-25 16:59:25 +0530

    Update the routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Reservations from './components/Reservations'
+import MyReservation from './components/MyReservation'
+import Details from './components/Details';
+
+import Login from './components/Login';
+import Signup from './components/Signup';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login />
       <Home />
-    </>
+      <Signup />
+    </> */}
+    <BrowserRouter>
+    <Login />
+    <Routes>
+      <Route path="/" element={(<Home />)} />
+      <Route path="/reservations" element={(<Reservations />)} />
+      <Route path="/myreservation" element={(<MyReservation />)} />
+      <Route path="/details" element={(<Details />)} />
+      <Route path="/signup" element={(<Signup />)} />
+    </Routes>
+  </BrowserRouter>
+</>
   );
 }
 
