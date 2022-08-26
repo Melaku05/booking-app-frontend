1df37d6ebe11c18487076411cf4cb37e7fc945f1
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-26 02:56:13 -0500

    Add navigation for reservation page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation';
+import NavigationReserve from './components/NavigationReserve';
+// import Navigation from './components/Navigation';
+import Reservations from './components/Reservations';
 
 function App() {
   return (
     <>
-      <Navigation />
-      <h1>Hello world! The Navigation views are Updated</h1>
+      {/* <Navigation /> */}
+      <NavigationReserve />
+      <Reservations />
     </>
   );
 }
