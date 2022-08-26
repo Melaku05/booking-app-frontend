751cbf2e2c51678fbfa42fb83ff10f21fe33e450
Ismail Courr <contact@ismailcourr.ml>
2022-08-26 15:49:02 +0100

    redirect to the login page when the user in signout

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 /* Install pure-react-carousel using -> npm i pure-react-carousel */
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signOut } from '../redux/login/signout';
 
@@ -11,6 +11,12 @@ const Navigation = () => {
   const dispatch = useDispatch();
 
   const userInfo = JSON.parse(localStorage.getItem('user'));
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    dispatch(signOut());
+    navigate('/');
+  };
+
   return (
     <div>
       <div className="flex items-center justify-between w-full p-6 border-b border-transparent rounded-r shadow bg-navbar xl:hidden sm:border-gray-200 ">
@@ -97,7 +103,7 @@ const Navigation = () => {
             </svg>
             <p className="font-bold leading-4 text-black border-b border-transparent hover:border-doctor focus:border-white ">MY RESERVATIONS</p>
           </Link>
-          <button onClick={() => dispatch(signOut())} type="button" className="flex items-center justify-start w-full py-3 space-x-6 font-bold text-red-500 rounded focus:outline-none focus:text-700 hover:text-red-600 ">
+          <button onClick={() => handleSignOut()} type="button" className="flex items-center justify-start w-full py-3 space-x-6 font-bold text-red-500 rounded focus:outline-none focus:text-700 hover:text-red-600 ">
             <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M14 8V6C14 5.46957 13.7893 4.96086 13.4142 4.58579C13.0391 4.21071 12.5304 4 12 4H5C4.46957 4 3.96086 4.21071 3.58579 4.58579C3.21071 4.96086 3 5.46957 3 6V18C3 18.5304 3.21071 19.0391 3.58579 19.4142C3.96086 19.7893 4.46957 20 5 20H12C12.5304 20 13.0391 19.7893 13.4142 19.4142C13.7893 19.0391 14 18.5304 14 18V16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
               <path d="M7 12H21M21 12L18 9M21 12L18 15" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
