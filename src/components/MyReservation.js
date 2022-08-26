f5e20e30f90c4cceed609248e2295da1a8d91bc8
Ismail Courr <contact@ismailcourr.ml>
2022-08-26 20:02:51 +0100

    Display my reservaations data on the page

--- src/components/MyReservation.js
+++ src/components/MyReservation.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMyReservations } from '../redux/myReservations/myReservations';
+import { getDoctors } from '../redux/doctors/doctors';
 
-const MyReservation = () => (
-  <div>My Reservation</div>
-);
+const MyReservation = () => {
+  const myReservations = useSelector((state) => state.myReservations);
+  const doctor = useSelector((state) => state.doctor);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMyReservations());
+    dispatch(getDoctors());
+  }, []);
+
+  return (
+    <>
+      {myReservations.data.error && <h1>{myReservations.data.error}</h1>}
+      {myReservations.data.error === undefined && (
+        <div>
+          {myReservations.data.map((reservation) => (
+            <div key={reservation.id}>
+              <p>{doctor.data[reservation.doctor_id - 1].name}</p>
+              <img src={doctor.data[reservation.doctor_id - 1].photo} alt="doctor" width="120px" />
+              <p>{reservation.city}</p>
+              <p>{reservation.date}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default MyReservation;
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import logger from 'redux-logger';
 import signinReducer from './login/signin';
 import signUpReducer from './login/signup';
 import doctorReducer from './doctors/doctors';
+import myReservationsReducer from './myReservations/myReservations';
 
 const store = configureStore({
   reducer: {
     login: signinReducer,
     signup: signUpReducer,
     doctor: doctorReducer,
+    myReservations: myReservationsReducer,
   },
   middleware: [thunk, logger],
 });
