be8fba3a67876b52a554a12cb90bbfd8706e7979
ismail courr <contact@ismailcourr.ml>
2022-08-30 13:57:08 +0100

    Update ApiUrl.js
    - Change the url to the live one (https://booking-app.fly.dev/)

--- src/helpers/ApiUrl.js
+++ src/helpers/ApiUrl.js
@@ -1,5 +1,5 @@
 // Enter the URL of your API here like so: const url = 'http://localhost:3000/';
 // The "/" at the end is important!
-const url = 'http://localhost:3000/';
+const url = 'https://booking-app.fly.dev/';
 
 export default url;
