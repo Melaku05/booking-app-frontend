8afade6fa276fa9f563abceacac89893f3cd9065
melaku05 <melakushiferaw05@gmail.com>
2022-08-31 16:03:33 +0300

    Add login routes into app.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,23 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import './App.css';
 import ProtectedRoutes from './helpers/ProtectedRoutes';
+import LoginRoutes from './helpers/LoginRoutes';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Login />} />
+          <Route element={<LoginRoutes />}>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+          </Route>
           <Route element={<ProtectedRoutes />}>
             <Route exact path="/home" element={<Home />} />
             <Route exact path="/reservations" element={<Reservations />} />
             <Route exact path="/myreservation" element={<MyReservation />} />
             <Route exact path="/details/:id" element={<Details />} />
           </Route>
-          <Route exact path="/signup" element={<Signup />} />
           <Route path="*" element={<h1 className="font-bold text-2xl text-center mt-20">404 Page Not Found</h1>} />
         </Routes>
       </BrowserRouter>
