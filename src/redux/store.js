70d652d4d08bd4102fdd5ea728e9c079b4cfd33a
melaku05 <melakushiferaw05@gmail.com>
2022-08-31 16:12:01 +0300

    Remove signup link from routes

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import signinReducer from './login/signin';
-import signUpReducer from './login/signup';
 import doctorReducer from './doctors/doctors';
 import myReservationsReducer from './myReservations/myReservations';
 import reservationsReducer from './Reservations/Reservations';
@@ -10,7 +9,6 @@ import reservationsReducer from './Reservations/Reservations';
 const store = configureStore({
   reducer: {
     login: signinReducer,
-    signup: signUpReducer,
     doctor: doctorReducer,
     myReservations: myReservationsReducer,
     reservations: reservationsReducer,
